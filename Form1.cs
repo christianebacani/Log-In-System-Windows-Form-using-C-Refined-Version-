c62c80beeca76817d012d1a03650570d9bd25f7b
christianebacani <121742592+christianebacani@users.noreply.github.com>
2024-05-12 22:00:48 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text=="Christiane" && txtpassword.Text=="BSCS2C")
+            {
+                new Form2().Show();
+                this.Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("The Username or Password you entered is incorrect!, Try again");
+                txtUsername.Clear();
+                txtpassword.Clear();
+                txtUsername.Focus();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            txtUsername.Clear();
+            txtpassword.Clear();
+            txtUsername.Focus();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
